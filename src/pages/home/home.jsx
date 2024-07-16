39a083a3944775f0184990d31a0947b547d52272
Timi <tunmisebabade@gmail.com>
2024-07-16 05:26:37 +0100

    first commit

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { Link, Navigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 import { AuthContext } from '../../context/authContext'
 import { Layout } from '../../components/custom/layout'
 import { Button } from '../../components/ui/button'
